0ae5e74171b1bc8cff8e67e7c9052c802742aea0
Daniel <danielcarrilloharris@gmail.com>
2015-09-17 11:25:00 +0100

    Create media.py

---
+++ media.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+#Create class movie for usage in entertainment center
+
+class Movie():
+    def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube_url, rating, genre, director, date, actors):
+        self.title = movie_title
+        self.storyline = movie_storyline
+        self.posterImageUrl = poster_image
+        self.trailerYoutubeUrl = trailer_youtube_url
+        self.rating = rating
+        self.genre = genre
+        self.date = date
+        self.director = director
+        self.actors = actors
