b688156f11a0c5cbb8e98d3e97b64f6a872d0124
Daniel <danielcarrilloharris@gmail.com>
2015-09-24 12:57:13 +0100

    Update media.py

--- media.py
+++ media.py
@@ -3,7 +3,9 @@
 #Create class movie for usage in entertainment center
 
 class Movie():
-    def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube_url, rating, genre, director, date, actors):
+    def __init__(self, movie_title, movie_storyline, poster_image,
+                trailer_youtube_url, rating, genre, director, date,
+                actors):
         self.title = movie_title
         self.storyline = movie_storyline
         self.posterImageUrl = poster_image
